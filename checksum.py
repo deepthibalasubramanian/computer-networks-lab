1543f717fd74891100628c25efac7f8794c06a91
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-04-24 15:32:14 +0530

    Update checksum.py

--- checksum.py
+++ checksum.py
@@ -27,3 +27,16 @@ print(x)
 
 
 
+arr=["4500","003C","1C46","4000","4006","0000","AC10","0A63","AC10","0A0C"]
+n=10
+temp="4500"
+for i in range(1,n):
+    temp=hex(int(temp,16)+int(arr[i],16))
+print(temp[2:])
+z=len(temp)
+a=temp[2:(z-4)]
+b=temp[(z-4):]
+
+x=hex(int(a,16)+int(b,16))
+#y=hex(int(x,16)+int("0xff",16))
+print(x)

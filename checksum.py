4de7e38bcb82336dbe353dee73a86c194b2ae323
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-04-30 09:03:48 +0530

    Update checksum.py

--- checksum.py
+++ checksum.py
@@ -1,42 +1,16 @@
 #created on 24.4.23
-
-arr=["4500","003C","1C46","4000","4006","0000","AC10","0A63","AC10","0A0C"]
-n=10
-temp="4500"
-for i in range(1,n):
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(input())
+temp='0x0000'
+for i in range(n):
     temp=hex(int(temp,16)+int(arr[i],16))
-print(temp[2:])
-
-x=hex(int("4e17",16)+int("2",16))
-y=hex(int(x,16)+int("0xff",16))
-print(y)
-
-
-
-arr=["4500","003C","1C46","4000","4006","0000","AC10","0A63","AC10","0A0C"]
-n=10
-temp="4500"
-for i in range(1,n):
-    temp=hex(int(temp,16)+int(arr[i],16))
-print(temp[2:])
-print(len(temp))
-
-x=hex(int(temp[2],16)+int(temp[3:],16))
-#y=hex(int(x,16)+int("0xff",16))
-print(x)
-
-
-
-arr=["4500","003C","1C46","4000","4006","0000","AC10","0A63","AC10","0A0C"]
-n=10
-temp="4500"
-for i in range(1,n):
-    temp=hex(int(temp,16)+int(arr[i],16))
-print(temp[2:])
 z=len(temp)
 a=temp[2:(z-4)]
 b=temp[(z-4):]
 
-x=hex(int(a,16)+int(b,16))
-#y=hex(int(x,16)+int("0xff",16))
-print(x)
+x=int(a,16)+int(b,16)
+x=~x
+y=hex(x&0xFFFF)[2:]
+print(y)

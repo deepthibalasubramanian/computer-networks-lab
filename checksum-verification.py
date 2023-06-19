427605c0ef2761d569950b4640030a9e50bf1fff
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-06-19 15:13:58 +0530

    Update checksum-verification.py

--- checksum-verification.py
+++ checksum-verification.py
@@ -63,3 +63,86 @@ else:'''
 
 # total devices
 print("Number of devices: "+str(totaldevice))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''# given:
+# IP address
+# net mask
+# to find:
+# network address: first address
+# last address
+# total devices
+
+# idea: convert all binary values into 8 bit format
+# join them together
+# find subnet mask in binary
+# do AND for first address
+# take 1's complement and OR for last address
+    
+
+ipaddress=input()
+array=ipaddress.split('.')
+array2=array[-1].split('/')
+#print(array2)
+array.pop()
+array.extend(array2)
+print(array)
+
+# network address
+# decimal to binary
+#bin(n).replace("0b", "")
+array1=[]
+netmask=int(array[-1])
+hostaddress=32-netmask
+totaldevice=2**hostaddress
+inputset=''
+array3=array
+array.pop()
+#format(x, '08b')
+for i in array:
+    #x=(bin(int(i)).replace("0b", ""))
+    y=format(int(i),'08b')
+    array1.append(y)
+    inputset=inputset+y
+    
+print(array1)
+print(inputset)
+subnet=''
+for i in range(int(netmask)):
+    subnet=subnet+'1'
+for i in range(int(hostaddress)):
+    subnet=subnet+'0'
+print(subnet)
+
+firstaddbin=bin(subnet)&bin(inputset)
+print(int(firstaddbin))
+
+
+# total devices
+print("Number of devices: "+str(totaldevice))'''

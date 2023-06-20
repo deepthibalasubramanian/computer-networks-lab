c86601b36e8bb945b188a683797b336c4f417eda
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-06-20 22:29:40 +0530

    Update First-address.py

--- First-address.py
+++ First-address.py
@@ -11,7 +11,7 @@
 # find subnet mask in binary
 # do AND for first address
 # take 1's complement and OR for last address
-    
+
 
 ipaddress=input()
 array=ipaddress.split('.')
@@ -19,7 +19,7 @@ array2=array[-1].split('/')
 #print(array2)
 array.pop()
 array.extend(array2)
-print(array)
+#print(array)
 
 # network address
 # decimal to binary
@@ -37,30 +37,36 @@ for i in array:
     y=format(int(i),'08b')
     array1.append(y)
     inputset=inputset+y
-print(array1)
-print(inputset)
+#print(array1)
+#print(inputset)
 subnet=''
 for i in range(int(netmask)):
     subnet=subnet+'1'
 for i in range(int(hostaddress)):
     subnet=subnet+'0'
-print(subnet)
+#print(subnet)
 firstaddbin=''
 
 for i in range(32):
     firstaddbin+=str(int(subnet[i])&int(inputset[i]))
 #firstaddbin=bin(subnet)&int(inputset)
-print(firstaddbin)
+#print(firstaddbin)
 firstaddress=(str(int(firstaddbin[:8],2))+'.'+str(int(firstaddbin[8:16],2))+'.'+str(int(firstaddbin[16:24],2))+'.'+str(int(firstaddbin[24:32],2)))
 print(firstaddress)
 compsubnet=''
+
 #last address
-a=int(subnet,2)
-a=~a
-print(bin(a))
-for i in subnet:
-    compsubnet+=str(~int(i))
-print(compsubnet)
+for i in range(32):
+    if(subnet[i]=='0'):
+        compsubnet+='1'
+    else:
+        compsubnet+='0'
+#print(compsubnet)
+lastaddbin=''
+for i in range(32):
+    lastaddbin+=str(int(compsubnet[i])|int(inputset[i]))
+lastaddress=(str(int(lastaddbin[:8],2))+'.'+str(int(lastaddbin[8:16],2))+'.'+str(int(lastaddbin[16:24],2))+'.'+str(int(lastaddbin[24:32],2)))
+print(lastaddress)
 
 # total devices
-print("Number of devices: "+str(totaldevice))
+print(str(totaldevice))

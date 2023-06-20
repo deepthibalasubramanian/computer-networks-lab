e48aa768fbffd3fbd31aafd6ddd241b61a6e90cf
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-06-20 22:39:18 +0530

    Update First-address.py

--- First-address.py
+++ First-address.py
@@ -12,45 +12,35 @@
 # do AND for first address
 # take 1's complement and OR for last address
 
-
 ipaddress=input()
 array=ipaddress.split('.')
 array2=array[-1].split('/')
-#print(array2)
 array.pop()
 array.extend(array2)
-#print(array)
-
-# network address
-# decimal to binary
-#bin(n).replace("0b", "")
 array1=[]
+
 netmask=int(array[-1])
 hostaddress=32-netmask
 totaldevice=2**hostaddress
 inputset=''
 array3=array
 array.pop()
-#format(x, '08b')
+
 for i in array:
-    #x=(bin(int(i)).replace("0b", ""))
     y=format(int(i),'08b')
     array1.append(y)
     inputset=inputset+y
-#print(array1)
-#print(inputset)
+
 subnet=''
 for i in range(int(netmask)):
     subnet=subnet+'1'
 for i in range(int(hostaddress)):
     subnet=subnet+'0'
-#print(subnet)
-firstaddbin=''
 
+#first address
+firstaddbin=''
 for i in range(32):
     firstaddbin+=str(int(subnet[i])&int(inputset[i]))
-#firstaddbin=bin(subnet)&int(inputset)
-#print(firstaddbin)
 firstaddress=(str(int(firstaddbin[:8],2))+'.'+str(int(firstaddbin[8:16],2))+'.'+str(int(firstaddbin[16:24],2))+'.'+str(int(firstaddbin[24:32],2)))
 print(firstaddress)
 compsubnet=''
@@ -61,7 +51,6 @@ for i in range(32):
         compsubnet+='1'
     else:
         compsubnet+='0'
-#print(compsubnet)
 lastaddbin=''
 for i in range(32):
     lastaddbin+=str(int(compsubnet[i])|int(inputset[i]))
